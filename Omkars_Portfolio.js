d845002c498d15d7704bff3e8994a7c1cf726305
omkar <degavathomkar934@gmail.com>
2024-12-09 16:24:42 +0530

    fist commit

---
+++ Omkars_Portfolio.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#dynamic-text", {
+    strings: ["Developer", "Full Stack Developer", "Software Engineer"],
+    typeSpeed: 60, // Speed at which each letter types out
+    backSpeed: 20, // Speed at which it erases
+    backDelay: 1000, // Delay before typing the next word
+    startDelay: 500, // Initial delay before it starts typing
+    loop: true, // Make it loop through the words
+});
